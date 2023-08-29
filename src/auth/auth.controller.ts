cb9e5ad2789f5e9a29ca47557318deeb97b4ea42
George <g3orrg301@gmail.com>
2023-08-30 01:07:22 +0300

    Tested sign up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Get()
+    getTitle(): string {
+        return this.authService.getTitle();
+    }
+    
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+import { auth } from 'firebase-config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    getTitle(): string {
+        createUserWithEmailAndPassword(auth, 'gddd3orrg301@gmail.com', 'Ienupar3')
+            .then(userCredential => {
+                const user = userCredential.user;
+                console.log(user)
+            })
+            .catch(error => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            })
+        
+        return 'auth route';
+    }
+}
